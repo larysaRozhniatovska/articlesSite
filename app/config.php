49595010a16b3f8e61fee9b82239248c0d4d813f
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-11-22 01:07:08 +0200

    edit config.php

--- app/config.php
+++ app/config.php
@@ -1,1 +1,23 @@
 <?php
+const TEXT_SITE_NAME = 'Articles';
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = 'jul_articles_site';
+const ERROR_LOGIN = 'Incorrect login or password. Please try ago';
+
+//DIR===========================================================
+const LIB_DIR = APP_DIR . 'lib' . DIRECTORY_SEPARATOR;
+const VIEWS_DIR = APP_DIR . 'views' . DIRECTORY_SEPARATOR;
+const VIEWS_COMMON_DIR = VIEWS_DIR . 'common' . DIRECTORY_SEPARATOR;
+const VIEWS_PAGES_DIR = VIEWS_DIR . 'pages' . DIRECTORY_SEPARATOR;
+
+const VIEWS_TEMPLATES_DIR = VIEWS_DIR . 'templates' . DIRECTORY_SEPARATOR;
+const JS_DIR = 'js' . DIRECTORY_SEPARATOR;
+
+//css all parameters================================================================
+const CSS_DIR = 'css' . DIRECTORY_SEPARATOR;
+
+const IMAGES_DIR = DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR;
+const IMAGES = IMAGES_DIR . 'background.jpg';
+const VIEWS_TEMPLATE = 'default';
